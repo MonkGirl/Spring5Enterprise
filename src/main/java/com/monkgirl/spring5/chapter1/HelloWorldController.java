69f88c56461bb10a060e249fcfb17c87048cf8be
MonkGirl <monkgirl2014@gmail.com>
2020-04-04 21:29:08 +0800

    init

--- src/main/java/com/monkgirl/spring5/chapter1/HelloWorldController.java
+++ src/main/java/com/monkgirl/spring5/chapter1/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.monkgirl.spring5;
+package com.monkgirl.spring5.chapter1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/com/monkgirl/spring5/chapter2/Eatable.java
@@ -0,0 +1,12 @@
+package com.monkgirl.spring5.chapter2;
+
+/**
+ * @author MissYoung
+ * @version 0.1
+ * @description
+ * @date 2020-04-04 20:07
+ */
+public interface Eatable {
+
+    void eat();
+}
