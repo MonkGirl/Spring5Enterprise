e3d565935e0b461af5bfd895900bb03b9c54a1ac
MonkGirl <monkgirl2014@gmail.com>
2020-04-04 19:45:02 +0800

    init

---
+++ src/main/java/com/monkgirl/spring5/Demo.java
@@ -0,0 +1,9 @@
+package com.monkgirl.spring5;
+/**
+ * @author MissYoung
+ * @version 0.1
+ * @description
+ * @date 2020-04-04 18:31
+ */
+public class Demo {
+}
---
+++ src/main/java/com/monkgirl/spring5/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.monkgirl.spring5;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MissYoung
+ * @version 0.1
+ * @description
+ * @date 2020-04-04 18:37
+ */
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @RequestMapping("hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
